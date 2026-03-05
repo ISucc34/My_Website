cf98b9ab78b23c695ca7d2fe92442e8e6252ff1d
Isaac Kristopher Gonzalez <142045835+ISucc34@users.noreply.github.com>
2026-03-04 20:04:54 -0600

    update for one logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import TfLogo from './assets/TensorFlow.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const logos = {
+    Tf: {logo:'./assets/TensorFlow.svg', link:"https://www.tensorflow.org"},
+    PyCharm:"src/assets/PyCharm.svg",
+    Git: "src/assets/Git.svg",
+    github : "src/assets/GitHub.svg",
+    vsCode : "src/assets/Visual Studio Code (VS Code).svg"
+  };
+
+
   return (
     <>
       <div>
+        <h1>Isaac's Portfolio</h1>
+
+        <h2>Technologies</h2>
+        <div >
+          <img src={TfLogo} className="logoContainer" alt="Tensorflow logo" />
+        </div>
+
+        <h2>Libraries/Frameworks</h2>
+        <h2> Projects</h2>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={TfLogo} className="logo" alt="Tensorflow logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>My Portfolio</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
