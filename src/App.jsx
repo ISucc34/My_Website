5451f9dcc55e0a70df2ef613cb5cb060eeaecc4b
Isaac Kristopher Gonzalez <142045835+ISucc34@users.noreply.github.com>
2026-03-08 13:06:09 -0500

    logos work side by side

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import TfLogo from './assets/TensorFlow.svg'
+import pyCharmLogo from './assets/PyCharm.svg'
+import gitLogo from './assets/Git.svg'
+import githubLogo from './assets/GitHub.svg'
+import vsCodeLogo from './assets/Visual Studio Code (VS Code).svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  const logos = {
-    Tf: {logo:'./assets/TensorFlow.svg', link:"https://www.tensorflow.org"},
-    PyCharm:"src/assets/PyCharm.svg",
-    Git: "src/assets/Git.svg",
-    github : "src/assets/GitHub.svg",
-    vsCode : "src/assets/Visual Studio Code (VS Code).svg"
-  };
+  const logos = [
+    { name: 'TensorFlow', src: TfLogo },
+    { name: 'PyCharm', src: pyCharmLogo },
+    { name: 'Git', src: gitLogo },
+    { name: 'GitHub', src: githubLogo },
+    { name: 'VS Code', src: vsCodeLogo},
+  ]
 
 
   return (
@@ -21,31 +23,22 @@ function App() {
         <h1>Isaac's Portfolio</h1>
 
         <h2>Technologies</h2>
-        <div >
-          <img src={TfLogo} className="logoContainer" alt="Tensorflow logo" />
+        <div className="logos-container">
+          {logos.map((logo) => (
+            <div key={logo.name}>
+              <img src={logo.src} className='logo' alt={`${logo.name} logo`} />
+            </div>
+          ))}
         </div>
+
+
 
         <h2>Libraries/Frameworks</h2>
         <h2> Projects</h2>
-        <a href="https://vite.dev" target="_blank">
-          <img src={TfLogo} className="logo" alt="Tensorflow logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
       </div>
       
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
